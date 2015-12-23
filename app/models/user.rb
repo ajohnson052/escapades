a9aa4387cb30971b6b06511439a0251727ecfd7b
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-23 08:18:52 -0600

    add inverse_tempships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_many :commits
   has_many :tempships
   has_many :temps, through: :tempships
+  has_many :inverse_tempships, class_name: "Tempship", foreign_key: "temp_id"
+  has_many :inverse_temps, through: :inverse_tempships, source: :user
   has_many :friendships
   has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
