10528361649331e5dfe52ed7d7755b5c5a66ccf1
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-19 15:46:34 -0500

    generate escapades controller, helper, test etc

---
+++ app/helpers/escapades_helper.rb
@@ -0,0 +1,2 @@
+module EscapadesHelper
+end
---
+++ test/controllers/escapades_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EscapadesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
