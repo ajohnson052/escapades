3cdc5753eaeefe59eb171972a36bd6e718946aa1
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-20 11:37:41 -0500

    extract get_user_availability from get_optimal_dates

--- app/helpers/escapades_helper.rb
+++ app/helpers/escapades_helper.rb
@@ -1,12 +1,17 @@
 module EscapadesHelper
 
+  def get_user_availability response
+    user_availability = []
+    response.availabilities.each do |availability|
+      user_availability = user_availability + (availability.start_date..availability.end_date).to_a
+    end
+    user_availability
+  end
+
   def get_optimal_dates escapade
     optimal_dates = (escapade.start_date..escapade.end_date).to_a
     escapade.responses.each do |response|
-      user_availability = []
-      response.availabilities.each do |availability|
-        user_availability = user_availability + (availability.start_date..availability.end_date).to_a
-      end
+      user_availability = get_user_availability response
       optimal_dates = optimal_dates & user_availability unless user_availability.empty?
     end
     optimal_dates = optimal_dates.reject do |i|
@@ -14,5 +19,5 @@ module EscapadesHelper
     end
     optimal_dates = optimal_dates.each_slice(2).to_a
   end
-  
+
 end
