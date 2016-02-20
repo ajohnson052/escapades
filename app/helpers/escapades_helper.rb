6f8ef92a5a96502c9ebc4ba815d6eaf8b0231ff8
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-20 12:30:47 -0500

    extract convert_to_range from get_optimal_dates

--- app/helpers/escapades_helper.rb
+++ app/helpers/escapades_helper.rb
@@ -8,15 +8,20 @@ module EscapadesHelper
     user_availability
   end
 
+  def convert_to_range optimal_dates
+    optimal_dates = optimal_dates.reject do |i|
+      optimal_dates.include?(i-1) & optimal_dates.include?(i+1)
+    end
+    optimal_dates
+  end
+
   def get_optimal_dates escapade
     optimal_dates = (escapade.start_date..escapade.end_date).to_a
     escapade.responses.each do |response|
       user_availability = get_user_availability response
       optimal_dates = optimal_dates & user_availability unless user_availability.empty?
     end
-    optimal_dates = optimal_dates.reject do |i|
-      optimal_dates.include?(i-1) & optimal_dates.include?(i+1)
-    end
+    optimal_dates = convert_to_range optimal_dates
     optimal_dates = optimal_dates.each_slice(2).to_a
   end
 
