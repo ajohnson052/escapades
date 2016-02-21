a3b59162254388a9e903b396ef79335afe9beb4e
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-21 08:46:00 -0500

    simplify method and variable names

--- app/helpers/escapades_helper.rb
+++ app/helpers/escapades_helper.rb
@@ -4,26 +4,31 @@ module EscapadesHelper
     (incident.start_date..incident.end_date).to_a
   end
 
-  def get_user_availability response
+  def get_availability response
     user_availability = response.availabilities.to_a
     user_availability.map! { |availability| get_dates(availability)}
     user_availability.flatten!
   end
 
-  def convert_to_range optimal_dates
-    optimal_dates = optimal_dates.reject do |i|
-      optimal_dates.include?(i-1) & optimal_dates.include?(i+1)
+  def convert_to_range dates
+    range = dates.reject do |i|
+      dates.include?(i-1) & dates.include?(i+1)
+    end
+    range
+  end
+
+  def all_available escapade
+    optimal_dates = get_dates(escapade)
+    escapade.responses.each do |response|
+      user_availability = get_availability(response)
+      optimal_dates = optimal_dates & user_availability unless user_availability.empty?
     end
     optimal_dates
   end
 
   def get_optimal_dates escapade
-    optimal_dates = get_dates(escapade)
-    escapade.responses.each do |response|
-      user_availability = get_user_availability response
-      optimal_dates = optimal_dates & user_availability unless user_availability.empty?
-    end
-    optimal_dates = convert_to_range optimal_dates
+    optimal_dates = all_available(escapade)
+    optimal_dates = convert_to_range(optimal_dates)
     optimal_dates = optimal_dates.each_slice(2).to_a
   end
 
